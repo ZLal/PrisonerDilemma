0bfd750da803ced157f6af97b55861280bae4413
ZLal <zeta.amrithlal@gmail.com>
2024-02-22 18:36:22 +0530

    Adding project

---
+++ AT_PrisonersDilemma/Bots/AlwaysCooperate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AT_PrisonersDilemma.Bots
+{
+    public class AlwaysCooperate : IBot
+    {
+        public string Name => "Always Cooperate";
+
+        public BotAction NextIteration() => BotAction.Cooperate;
+
+        public void Reset() { }
+        public void SetResult(BotAction player, BotAction opponent) { }
+    }
+}
---
+++ AT_PrisonersDilemma/Other.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AT_PrisonersDilemma
+{
+    public class StateItem
+    {
+        public IBot Player1 { get; set; }
+        public IBot Player2 { get; set; }
+
+        public List<KeyValuePair<BotAction, BotAction>> Results { get; set; }
+    }
+
+    public class ResultData
+    {
+        public List<KeyValuePair<string, int>> LeaderBoard { get; set; }
+        public List<ResultItem> Results { get; set; }
+    }
+
+    public class ResultItem
+    {
+        public string Player1 { get; set; }
+        public string Player2 { get; set; }
+
+        public List<Tuple<BotAction, BotAction, int, int>> ScoreList { get; set; }
+    }
+
+    public enum BotAction
+    {
+        Cooperate,
+        Defect
+    }
+
+    public interface IBot
+    {
+        public string Name { get; }
+
+        BotAction NextIteration();
+        void SetResult(BotAction player, BotAction opponent);
+        void Reset();
+    }
+}
